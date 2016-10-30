11c1d121ba96f79a01b23beaee71323ff1d7aa5c
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-10-30 17:25:58 +0100

    Add some TODOs

--- lib/eqlib.dart
+++ lib/eqlib.dart
@@ -2,6 +2,12 @@
 // Use of this source code is governed by an AGPL-3.0-style license
 // that can be found in the LICENSE file.
 
+/// TODO:
+/// - Store expressions as integers (keep String labels in a Map).
+/// - Store numeric values inside the Expr.
+/// - Make Expr final, operations ALWAYS produce a new instance.
+/// - Provide functions and operators for inline expression building.
+/// - Create a plain text format for derivations (to build a library).
 library eqlib;
 
 import 'package:quiver/core.dart';
