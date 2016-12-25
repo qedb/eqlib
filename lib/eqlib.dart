624b4a081baba925d3fc6f688f5c0c8a2730ddac
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-12-26 00:24:24 +0100

    Bump version to 0.1.1

--- lib/eqlib.dart
+++ lib/eqlib.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by an AGPL-3.0-style license
 // that can be found in the LICENSE file.
 
-/// TODO:
-/// - Recursive/conditional substitution
 library eqlib;
 
 import 'dart:math';
